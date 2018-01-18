caf7eb0b315694881ff4d42522c7b0c23b9a4e22
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-18 19:17:27 +0900

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,21 +1,20 @@
 <?php
-
 $db_host = "localhost";
 $db_user = "root";
 $db_pw = "111111";
 $db_name = "tutorials";
 $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
-
 if($connect)
   echo "db is connected";
 else
   echo "db is not connected";
-
-$result = mysqli_query($connect,"insert into info(song_name,song_artist,song_url)
-          value('$_POST[song_name]','$_POST[song_artist]','$_POST[song_url]')");
+if($_POST[reset.value]=="DB reset")
+	echo "reset";
+if($_POST[add.value]=="DB로 전송"){
+	$result = mysqli_query($connect,"insert into info(song_name,song_artist,song_url) value('$_POST[song_name]','$_POST[song_artist]','$_POST[song_url]')");
+	echo "add";
+	}
 echo $result;
 echo "<br>";
-
 mysqli_close($connect);
-
 ?>
