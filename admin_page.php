0b926560c5570c2b02a961bc409c57e9482fcb52
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-20 21:39:45 +0900

    Update admin_page.php

--- admin_page.php
+++ admin_page.php
@@ -3,9 +3,9 @@
 include './admin_login.php';
 
 if($_POST["isPassword"]==$pass){
-echo "true";
+    echo("<script>location.replace('./admin.html');</script>"); 
   }
   else{
-  echo "false";
+    echo("<script>location.replace('./index.html');</script>"); 
   }
 ?>
