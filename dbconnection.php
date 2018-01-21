04e805a2b9e5cb86ce81f14c304d71fd5e8a0640
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-21 17:28:11 +0900

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -5,14 +5,17 @@ $db_pw = "111111";
 $db_name = "tutorials";
 $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
 
+if(!$connect)
+  echo "ERROR! : failed to CONNECT server";
+
 $result = mysqli_query($connect,"insert into info(song_no, song_name,song_artist,song_url, song_category, song_date)
  value(default, '$_POST[song_name]','$_POST[song_artist]','$_POST[song_url]', '$_POST[song_category]', default)");
 
-
-if($connect)
+if($result)
   echo("<script>location.replace('./admin_insert.html');</script>"); 
 else
-  echo "server connect ERROR!";
+  echo "ERROR! : failed to SEND data";
+  
 
 echo "<br>";
 mysqli_close($connect);
