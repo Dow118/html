9473b3024a07ade21827e954d46af522ff575ca5
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-21 17:10:24 +0900

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -5,7 +5,8 @@ $db_pw = "111111";
 $db_name = "tutorials";
 $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
 
-$result = mysqli_query($connect,"insert into info(song_no, song_name,song_artist,song_url, song_date) value(default, '$_POST[song_name]','$_POST[song_artist]','$_POST[song_url]', default)");
+$result = mysqli_query($connect,"insert into info(song_no, song_name,song_artist,song_url, song_category, song_date)
+ value(default, '$_POST[song_name]','$_POST[song_artist]','$_POST[song_url]', '$_POST[song_category]', default)");
 
 
 if($connect)
