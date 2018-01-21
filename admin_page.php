babf582cc8621c24c97c6130c3e7ada8ad5b85fe
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-21 14:39:25 +0900

    Update admin_page.php

--- admin_page.php
+++ admin_page.php
@@ -3,7 +3,7 @@
 include './admin_login.php';
 
 if($_POST["isPassword"]==$pass){
-    echo("<script>location.replace('./admin.php');</script>"); 
+    echo("<script>location.replace('./admin_menu.html');</script>"); 
   }
   else{
     echo("<script>location.replace('./index.php');</script>"); 
