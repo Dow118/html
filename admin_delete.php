ea35881992d085a92249a091cdb4a30c60d84017
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-21 20:17:13 +0900

    Update admin_delete.php

--- admin_delete.php
+++ admin_delete.php
@@ -1,17 +1,14 @@
-
-                        <?php
+ <?php
                         $db_host = "localhost";
                         $db_user = "root";
                         $db_pw = "111111";
                         $db_name = "tutorials";
-                        $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);                       
-
-                        $result = mysqli_query($connect, "select * from info limit 0,10");
+                        $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+                        $result = mysqli_query($connect,"select * from info limit 0,10");
                           while($row=mysqli_fetch_row($result)){
                           echo "<form method='post' action='admin_delete_action.php'>;
-
                           echo "$row[0] | $row[1] | $row[2] | $row[3] | $row[4] | $row[5] ";
                           echo "<input type='submit' value='delete'/>";
                           echo "</form><br/>";
                           }
-                        ?>      
+?>     
