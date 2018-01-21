1dcce3df02130d75b613890ccd317fadaabd544d
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-21 16:19:11 +0900

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -4,11 +4,14 @@ $db_user = "root";
 $db_pw = "111111";
 $db_name = "tutorials";
 $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
-if($connect)
-  echo "db is connected!";
-else
-  echo "db is not connected...";
+
 $result = mysqli_query($connect,"insert into info(song_no, song_name,song_artist,song_url, song_date) value(default, '$_POST[song_name]','$_POST[song_artist]','$_POST[song_url]', default)");
+
+
+if($connect)
+  echo("<script>location.replace('./admin_insert.html');</script>"); 
+else
+  echo "server connect ERROR!";
 
 echo "<br>";
 mysqli_close($connect);
