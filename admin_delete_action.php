f11e45b415a55aade00db7bb860a55be560e9255
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-21 19:07:56 +0900

    Create admin_delete_action.php

---
+++ admin_delete_action.php
@@ -0,0 +1,26 @@
+<?php
+
+include './admin_delete.php';
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pw = "111111";
+$db_name = "tutorials";
+$connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+
+if(!$connect)
+  echo "ERROR! : failed to CONNECT server";
+  
+$result = mysqli_query($connect,"delete from info where song_no=$key");
+
+if(!$result)
+  echo "ERROR! : failed to SEND data";
+else{
+  mysqli_query("alter table info auto_increment=1");
+  mysqli_query("set @cnt=0");
+  mysqli_query("update info set info.song_no=@cnt:=@cnt+1");
+  echo("<script>location.replace('./admin_delete.php');</script>");
+  }
+
+
+?>
