abc0e8b58a2c1c6e990e1e2063331c2322aa0fe4
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-21 20:46:36 +0900

    Update admin_delete.php

--- admin_delete.php
+++ admin_delete.php
@@ -1,26 +1,9 @@
-<html>                        
-  <head>
-    <title> delete</title>
-  </head>
-  <body>
     <?
-     include './admin_delete_connect.php';
-    ?>
-    <br>
-    <br>
-    <br>
-    <a href="admin_delete.html?page=0"> 1 </a>
-    <a href="admin_delete.html?page=1"> 2 </a>
-    <a href="admin_delete.html?page=2"> 3 </a>
-    <a href="admin_delete.html?page=3"> 4 </a>
-    <a href="admin_delete.html?page=4"> 5 </a>
-  </body>
 
-</html>
+    include './admin_delete_page.html';
+    $page = $_GET['page'];
 
-
-
-                        <?php
+    if($page>-1){
                         $db_host = "localhost";
                         $db_user = "root";
                         $db_pw = "111111";
@@ -28,15 +11,16 @@
                         $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
                         $page_num = 0 + $_GET['page']*10;
                         $data_num = 20;
-                        ?>
-                <form method="POST" action = "admin_delete_action.php">
-                        <?php
+    echo "<form method='POST' action = 'admin_delete_action.php'>";
                         $result = mysqli_query($connect,"select * from info limit $start_num,$page_num");
                           while($row=mysqli_fetch_row($result)){
                           echo "<input type='hidden' name='key' value='$row[0]'/>";
                           echo "$row[0] | $row[1] | $row[2] | $row[3] | $row[4] | $row[5] ";
                           echo "<input type='submit' value='delete'/><br/>";
                           }
-                        ?> 
-                </form>
-    <br><br>
+    }
+    else
+      echo "no page<br>";
+
+    ?>
+
