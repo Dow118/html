2b8f038805df77ca484241436c82e17665ad87ad
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-22 15:39:38 +0900

    Create admin_modify_action.php

---
+++ admin_modify_action.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pw = "111111";
+$db_name = "tutorials";
+$connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+if(!$connect)
+  echo "ERROR! : failed to CONNECT server";
+else
+  echo "$_POST[key]";
+  
+
+?>
