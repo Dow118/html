c5d06a3f97960151c909f10685c4b451f367a088
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-22 20:20:45 +0900

    Create admin_delete_page_cal.php

---
+++ admin_delete_page_cal.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pw = "111111";
+$db_name = "tutorials";
+$connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+
+if(!$connect)
+  echo "ERROR! : failed to CONNECT server";
+  
+$result = mysqli_query($connect,"select song_no from info;");
+
+$total = mysqli_num_rows($result);
+
+echo $total;
+
+?>
