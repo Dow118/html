078be57efddd8c9b18fa82f0b68b040a04c9a6a1
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-22 16:41:19 +0900

    Update admin_modify_action.php

--- admin_modify_action.php
+++ admin_modify_action.php
@@ -9,11 +9,11 @@ if(!$connect)
   echo "ERROR! : failed to CONNECT server";
 
  $result = mysqli_query($connect,"select * from info where song_no=$_POST[key]");
- echo "<form method='post' action='admin_modify.php'>
+ echo "<form method='post' action='admin_modify.php'>";
  while($row=mysqli_fetch_row($result)){
                           echo "<input type='hidden' name='song_no' value='$row[0]'>";
                           echo "<label>곡    명 : </label><input type='text' name='song_artist' value='$row[1]'/><br>";
-                          echo "<label>아 티 스 트 : </label><input type='text' name='sonng_name' value='$row[2]'/><br>";
+                          echo "<label>아 티 스 트 : </label><input type='text' name='song_name' value='$row[2]'/><br>";
                           echo "<label>U R L : </label><input type='text' name='song_url' value='$row[3]'/><br>";
                           echo "<label>카 테 고 리 : </label><select name='song_category' />";
                           echo "<option value='' selected>--재 선택 --</option>"
@@ -22,6 +22,6 @@ if(!$connect)
                           echo "<option value='$row[0]'> $row[0] </option>";
                           }
  }
- echo "<br><input type='submit' value='수정하기'/><br>"
+ echo "<br><input type='submit' value='수정하기'/><br>";
 
 ?>
