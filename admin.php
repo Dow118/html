147665ab8de4726a411c0021cc5b22af13c2f14a
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-22 15:25:01 +0900

    Update admin.php

--- admin.php
+++ admin.php
@@ -7,9 +7,6 @@ switch($menu){
     echo("<script>location.replace('./admin_insert.html');</script>");
     break;
     case 'modify':
-    echo("<script>location.replace('./admin_modify.html');</script>");
-    break;
-    case 'delete':
     echo("<script>location.replace('./admin_delete.php');</script>");
     break;
 }
