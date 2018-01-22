cf2e2de8b2d66dd8546e4fe189b71784f67e5f1c
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-22 16:50:59 +0900

    Update admin_modify.php

--- admin_modify.php
+++ admin_modify.php
@@ -9,8 +9,7 @@ $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
 if(!$connect)
   echo "ERROR! : failed to CONNECT server";
   
-$result = mysqli_query($connect,"update info set song_artist='$_POST[song_artist]', song_name='$_POST[song_name]',
- song_url='$_POST[song_url]', song_category='$_POST[song_category'] where song_no='$_POST[song_no]'");
+$result = mysqli_query($connect,"update info set song_artist='$_POST[song_artist]', song_name='$_POST[song_name]', song_url='$_POST[song_url]', song_category='$_POST[song_category]' where song_no='$_POST[song_no]'");
  
   mysqli_query($connect,"alter table info auto_increment=1;");
   mysqli_query($connect,"set @cnt=0;");
