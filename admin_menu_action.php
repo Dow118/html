9d1575914cf3cb26298adf662f72aabd94372584
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-25 14:58:02 +0900

    Update admin_menu_action.php

--- admin_menu_action.php
+++ admin_menu_action.php
@@ -4,10 +4,10 @@ $menu = $_POST['menu'];
 
 switch($menu){
     case 'insert':
-    echo("<script>location.replace('./admin_insert.html');</script>");
+    echo("<script>location.replace('./admin_menu_insert.html');</script>");
     break;
     case 'modify':
-    echo("<script>location.replace('./admin_delete.php');</script>");
+    echo("<script>location.replace('./admin_menu_manage.html');</script>");
     break;
 }
 
