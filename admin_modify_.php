90b3e08cd350ee7d67cd0108f9190f061f799ce8
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-25 15:23:34 +0900

    Update and rename admin_modify.php to admin_modify_.php

--- admin_modify_.php
+++ admin_modify_.php
@@ -1,13 +1,6 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "root";
-$db_pw = "111111";
-$db_name = "tutorials";
-$connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
-
-if(!$connect)
-  echo "ERROR! : failed to CONNECT server";
+include './dbconnect.php';
 
  mysqli_query($connect, "set session character_set_connection=utf8;");
  mysqli_query($connect, "set session character_set_results=utf8;");
