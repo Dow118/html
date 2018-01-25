89c8324fc075b1d0ab883985d1ccd3a9aff48f2c
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-25 16:33:12 +0900

    Update delete_action.php

--- admin/menu/delete_action.php
+++ admin/menu/delete_action.php
@@ -10,7 +10,7 @@ else{
   mysqli_query($connect,"alter table info auto_increment=1;");
   mysqli_query($connect,"set @cnt=0;");
   mysqli_query($connect,"update info set info.song_no=@cnt:=@cnt+1;");
-  echo("<script>location.replace('./admin/manage.html');</script>");
+  echo("<script>location.replace('./admin/menu/manage.html');</script>");
   }
 
 
