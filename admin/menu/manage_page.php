b734f03b5e1d9976a3a1ff12c937bd70714d7ce7
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-25 16:32:19 +0900

    Update and rename admin/admin_menu_manage_page.php to admin/menu/manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -3,7 +3,7 @@
     $page = $_GET['page'];
 
     if($page>-1){
-                        include './dbconnect.php';
+                        include './html/dbconnect.php';
         
                         $page_num = $page*20;
                         $data_num = 20;
@@ -15,12 +15,12 @@
                         $result = mysqli_query($connect,"select * from info limit $page_num,$data_num");
         
                           while($row=mysqli_fetch_row($result)){
-                          echo "<form method='POST' action = 'admin_menu_delete_action.php'>";
+                          echo "<form method='POST' action = './delete_action.php'>";
                           echo "<input type='hidden' name='key' value='$row[0]'/>";
                           echo "$row[0] | $row[1] | $row[2] | $row[3] | $row[4] | $row[5] ";
                           echo "<input type='submit' value='delete' id='submitbutton'>";
                           echo "</form>";
-                          echo "<form method='POST' action = 'admin_menu_modify.html'>";
+                          echo "<form method='POST' action = './modify.html'>";
                           echo "<input type='hidden' name='key' value='$row[0]'>";
                           echo "<input type='submit' value='modify' id='submitbutton'/>";
                           echo "</form><br>";
