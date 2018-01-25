d1777882ab3587c91628a5507b459875faa887df
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-25 14:57:49 +0900

    Update and rename admin_delete.php to admin_menu_manage_page.php

--- admin_menu_manage_page.php
+++ admin_menu_manage_page.php
@@ -1,20 +1,13 @@
     <?php
 
-    include './admin_delete_page_head.html';
-
     $page = $_GET['page'];
 
     if($page>-1){
-                        $db_host = "localhost";
-                        $db_user = "root";
-                        $db_pw = "111111";
-                        $db_name = "tutorials";
-                        $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+                        include './dbconnect.php';
         
                         $page_num = $page*20;
                         $data_num = 20;
                         
-                          
                          mysqli_query($connect, "set session character_set_connection=utf8;");
                          mysqli_query($connect, "set session character_set_results=utf8;");
                          mysqli_query($connect, "set session character_set_client=utf8;");
@@ -22,12 +15,12 @@
                         $result = mysqli_query($connect,"select * from info limit $page_num,$data_num");
         
                           while($row=mysqli_fetch_row($result)){
-                          echo "<form method='POST' action = 'admin_delete_action.php'>";
+                          echo "<form method='POST' action = 'admin_menu_delete_action.php'>";
                           echo "<input type='hidden' name='key' value='$row[0]'/>";
                           echo "$row[0] | $row[1] | $row[2] | $row[3] | $row[4] | $row[5] ";
                           echo "<input type='submit' value='delete' id='submitbutton'>";
                           echo "</form>";
-                          echo "<form method='POST' action = 'admin_modify_action.php'>";
+                          echo "<form method='POST' action = 'admin_menu_modify.php'>";
                           echo "<input type='hidden' name='key' value='$row[0]'>";
                           echo "<input type='submit' value='modify' id='submitbutton'/>";
                           echo "</form><br>";
@@ -36,6 +29,5 @@
     else
       echo "select page below <br>";
 
-    include './admin_delete_page_foot.html';
     ?>
 
