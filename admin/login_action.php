011e9c36feb18615e2a9ce271a981e9c4d642b57
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-25 16:38:34 +0900

    Update login_action.php

--- admin/login_action.php
+++ admin/login_action.php
@@ -1,6 +1,6 @@
 <?php
 
-include './dbconnect.php';
+include '/dbconnect.php';
 
 // set Query to DB - get password
 $result = mysqli_query($connect,"select password from admin");
@@ -15,6 +15,6 @@ if($_POST["isPassword"]==$pass){
     echo("<script>location.replace('./menu.html');</script>"); 
   }
   else{
-    echo("<script>location.replace('./html/index.html');</script>"); 
+    echo("<script>location.replace('/index.html');</script>"); 
   }
 ?>
