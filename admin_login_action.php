28b837b20ba71abb167c35d93ca748b0a384814c
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-25 14:44:03 +0900

    Update admin_login_action.php

--- admin_login_action.php
+++ admin_login_action.php
@@ -4,6 +4,10 @@ include './dbconnect.php';
 
 $result = mysqli_query($connect,"select password from admin");
 
+  while($row = mysqli_fetch_row($result)){
+     $pass = $row[0];
+  }
+
 if($_POST["isPassword"]==$pass){
     echo("<script>location.replace('./admin_menu.html');</script>"); 
   }
