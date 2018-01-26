4acb64536b20bba6129c24dd1276094acdb96475
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-01-26 15:27:53 +0900

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -7,7 +7,10 @@
   $db_name = "tutorials";
   $connect = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
     
-  
+  mysqli_query($connect, "set session character_set_connection=utf8;");
+  mysqli_query($connect, "set session character_set_results=utf8;");
+  mysqli_query($connect, "set session character_set_client=utf8;");
+
   if(!$connect) 
     echo "ERROR! : failed to CONNECT server";
 
