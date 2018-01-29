80889593869ec14f7cd17f1ff73ba17d78854c3d
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-29 11:25:28 +0900

    Update manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -13,13 +13,13 @@
                           while($row=mysqli_fetch_row($result)){
                           echo "<form method='POST' action = './delete_action.php'>";
                           echo "<input type='hidden' name='key' value='$row[0]'/>";
-                          echo "<table class='type03'><tr><th scope='row'>번  호</th><th scope='row'>아티스트</th><th scope='row'>곡  명</th><th scope='row'>주소값</th><th>장 르</th><th scope='row'>등록일자</th></tr>";
+                          echo "<table class='type03'><tr><th>번  호</th><th>아티스트</th><th>곡  명</th><th>주소값</th><th>장 르</th><th>등록일자</th></tr>";
                           echo "<tr><td>$row[0] </td><td> $row[1] </td><td> $row[2] </td><td> $row[3] </td><td> $row[4] </td><td> $row[5] </td>";
-                          echo "<td><input type='submit' value='delete' id='submitbutton'></td>";
+                          echo "<td class='button'><input type='submit' value='delete' id='submitbutton'></td>";
                           echo "</form>";
                           echo "<form method='POST' action = './modify.html'>";
                           echo "<input type='hidden' name='key' value='$row[0]'>";
-                          echo "<td><input type='submit' value='modify' id='submitbutton'/></td>";
+                          echo "<td class='button'><input type='submit' value='modify' id='submitbutton'/></td>";
                           echo "</form></tr></table><br>";
                           }
     }
