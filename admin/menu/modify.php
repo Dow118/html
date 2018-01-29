bc88e7660fc89f5a90bb7c5a35c5bfa4f321b586
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-01-29 14:26:26 +0900

    Update modify.php

--- admin/menu/modify.php
+++ admin/menu/modify.php
@@ -11,9 +11,9 @@ include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
  echo "<form method='post' action='./modify_action.php'>";
  while($row=mysqli_fetch_row($result)){
                           echo "<input type='hidden' name='song_no' value='$row[0]'>";
-                          echo "<label>곡    명 : </label><input type='text' name='song_artist' value='$row[1]'/><br>";
+                          echo "<label>곡       명 : </label><input type='text' name='song_artist' value='$row[1]'/><br>";
                           echo "<label>아 티 스 트 : </label><input type='text' name='song_name' value='$row[2]'/><br>";
-                          echo "<label>U R L : </label><input type='text' name='song_url' value='$row[3]'/><br>";
+                          echo "<label>주       소 : </label><input type='text' name='song_url' value='$row[3]'/><br>";
                           echo "<label>카 테 고 리 : </label><select name='song_category' />";
                           echo "<option value='' selected>--재 선택 --</option>";
                              $result = mysqli_query($connect,"select distinct song_category from info");
