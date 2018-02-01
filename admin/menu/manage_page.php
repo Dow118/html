60a293434f2357403735a10c799467570fa18ced
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-01 17:10:05 +0900

    Update manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -9,9 +9,18 @@
                         $page_num = $page*20;
                         $data_num = 20;
         
-                        echo "<table class='type03' style='table-layout:fixed'><tr><th class='small'>
-                                <span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none';>번  호</span>
-                                <div style='display: none'><input type='radio'>HTML</div></div></th><th>아티스트</th><th class='large'>곡  명</th><th>주소값</th><th class='small'>장 르</th><th>등록일자</th></tr>";
+                        echo "<table class='type03' style='table-layout:fixed'>
+                        <tr>
+                        <th class='small' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none';>번  호</th>
+                        <div style='display: none'>
+                        내용
+                        </div>
+                        <th>아티스트</th>
+                        <th class='large'>곡  명</th>
+                        <th>주소값</th>
+                        <th class='small'>장 르</th>
+                        <th>등록일자</th>
+                        </tr>";
         
                         if($order_number==0){
                             $result = mysqli_query($connect,"select * from info order by song_no asc limit $page_num,$data_num");
