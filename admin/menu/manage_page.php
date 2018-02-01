60a5319cfde7f19a3a9733b870d8f1d659abb93e
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-01 17:31:21 +0900

    Update manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -9,15 +9,9 @@
                         $page_num = $page*20;
                         $data_num = 20;
         
-                        echo "<table class='type03' style='table-layout:fixed'>
-                        <tr>
-                        <th class='small'><span id='test' style='CURSOR: hand' onclick=\"if(plain.style.display==\"none\"){plain.style.display='';test.innerText='번  호▲';}else{plain.style.display='none';test.innerText='번  호▼';}\">번  호▼</span><div id='plain' style='display: none'><HR> <input type='checkbox'>오름차순<input type='checkbox'>내림차순<HR></div></th><th>아티스트</th><th class='large'>곡  명</th><th>주소값</th><th class='small'>장 르</th><th>등록일자</th></tr>";</th>
-                        <th>아티스트</th>
-                        <th class='large'>곡  명</th>
-                        <th>주소값</th>
-                        <th class='small'>장 르</th>
-                        <th>등록일자</th>
-                        </tr>";
+                        echo "<table class='type03' style='table-layout:fixed'><tr>
+                        <th class='small'><span id='test' style='CURSOR: hand' onclick=\"if(plain.style.display==\"none\"){plain.style.display='';test.innerText='번  호▲';}else{plain.style.display='none';test.innerText='번  호▼';}\">번  호▼</span><div id='plain' style='display: none'><HR> <input type='checkbox'>오름차순<input type='checkbox'>내림차순<HR></div></th>
+                        <th>아티스트</th><th class='large'>곡  명</th><th>주소값</th><th class='small'>장 르</th><th>등록일자</th></tr>";
         
                         if($order_number==0){
                             $result = mysqli_query($connect,"select * from info order by song_no asc limit $page_num,$data_num");
