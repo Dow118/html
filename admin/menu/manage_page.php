79a89ac4aada1fce3529bb17deee7f0e5ea1d218
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-05 16:11:27 +0900

    Update manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -3,20 +3,26 @@
     $page = $_GET['page'];
     
 
-    if(!$_GET['sortByNum']){
-        $order_number = 'asc';
+    if(!$_GET['sortBy']){
+        $sortBy = 'song_no';
     }   
     else{
-        $order_number = $_GET['sortByNum'];
+        $sortBy = $_GET['sortBy'];
     }
-    
-    if(!$_GET['sortByArtist']){
-        $order_artist = 'null';
+
+    if(!$_GET['order']){
+        $order = 'asc';
     }
     else{
-        $order_artist = $_GET['sortByArtist'];
+        $sortBy = $_GET['order'];
     }
     
+    if(!$_GET['filterByArtist']){
+        $filter_artist = 'null';
+    }
+    else{
+        $filter_artist = $_GET['filterByArtist'];
+    }
 
     if($page>-1){
                         include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
@@ -25,7 +31,7 @@
                         $data_num = 20;
         
                         echo "<table class='type03' style='table-layout:fixed'><tr><th class='small'>
-                                <span id=$order_number style='CURSOR: hand' onclick=\"this.id=(this.id=='asc')?'desc':'asc'; location.href='./manage.html?page=$page&sortByNum='+this.id+'&sortByArtist=$order_artist'\">번  호</span></th>
+                                <span id=$order style='CURSOR: hand' onclick=\"this.id=(this.id=='asc')?'desc':'asc'; location.href='./manage.html?page=$page&order='+this.id+'&sortBy=song_no&filterByArtist=$filter_artist'\">번  호</span></th>
                                 <th><span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none'; >아티스트</span><div style='display: none'>";
         
                         $result = mysqli_query($connect, "select distinct song_artist from info");
@@ -34,19 +40,26 @@
                             echo "<input type='radio' id='$row[0]' value='$row[0]' onclick='checkbox_event(this);'> $row[0]<br>";
                         }           
                         echo "</div></th>
-                                <th class='large'>곡  명</th>
+                                <th class='large'>
+                                <span id=$order style='CURSOR: hand' onclick=\"this.id=(this.id=='asc')?'desc':'asc'; location.href='./manage.html?page=$page&order='+this.id+'&sortBy=song_name&filterByArtist=$filter_artist\">곡  명</span></th>
                                 <th>주소값</th>
-                                <th class='small'>장 르</th>
+                                <th class='small'><span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none'; >장 르</span><div style='display: none'>";
+        
+                        $result = mysqli_query($connect, "select distinct song_category from info");
+        
+                        while($row=mysqli_fetch_row($result)){
+                            echo "<input type='radio' id='$row[0]' value='$row[0]' onclick='checkbox_event(this);'> $row[0]<br>";
+                        }    
+                        echo "</div></th>
                                 <th>등록일자</th></tr>";
                         
                         $query = "select * from info";
         
-                        if($order_artist != 'null'){
+                        if($filter_artist != 'null'){
                             $query2 = " where song_artist='$order_artist' ";
                         }
                         else $query2 = " ";
-                        
-                        $query3 = "order by song_no $order_number limit $page_num,$data_num";
+                        $query3 = "order by $sortBy $order limit $page_num,$data_num";
                         $query_result = $query.$query2.$query3;
         
                         $result = mysqli_query($connect, $query_result);
