7d603d0cb97fc7201623ae15ecc224232580d25d
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-05 13:09:45 +0900

    Update manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -1,31 +1,20 @@
 
     <?php
     $page = $_GET['page'];
-
-    if(!$_GET['sortByNum']){
-        $order_number = 'asc';
-    }   
-    else{
-        $order_number = $_GET['sortByNum'];
-    }
-
     if($page>-1){
                         include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
         
                         $page_num = $page*20;
                         $data_num = 20;
         
-                        echo "<table class='type03' style='table-layout:fixed'><tr><th class='small'><span id=$order_number style='CURSOR: hand'  
-                                onclick=\"this.id=(this.id=='asc')?'desc':'asc'; location.href='./manage.html?page=$page&sortByNum='+this.id\">번  호</span> </th>
-                                <th><span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none';>아티스트</span>
-                                <div style='display: none'><input type='checkbox'>오름차순<input type='checkbox'>내림차순</div></th>
+                        $result = mysqli_query($connect,"select * from info limit $page_num,$data_num");
+                        echo "<table class='type03' style='table-layout:fixed'><tr><th class='small'>
+                                <span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none';>번  호</span><div style='display: none'><HR> <input type='checkbox'>오름차순<input type='checkbox'>내림차순<HR></div></th>
+                                <th><span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none';>아티스트</span><div style='display: none'><HR> <input type='checkbox'>오름차순<input type='checkbox'>내림차순<HR></div></th>
                                 <th class='large'>곡  명</th>
                                 <th>주소값</th>
                                 <th class='small'>장 르</th>
                                 <th>등록일자</th></tr>";
-        
-                        $result = mysqli_query($connect,"select * from info order by song_no $order_number limit $page_num,$data_num");
-        
                           while($row=mysqli_fetch_row($result)){
                           echo "<form method='POST' action = './delete_action.php'>";
                           echo "<input type='hidden' name='key' value='$row[0]'/>";  
