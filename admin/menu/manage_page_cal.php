abc77066ee1687a290fb565015a9c2edf5519845
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-05 16:09:21 +0900

    Update manage_page_cal.php

--- admin/menu/manage_page_cal.php
+++ admin/menu/manage_page_cal.php
@@ -13,22 +13,33 @@ while($total>-1){
   $i = $i + 1;
 }
 
-if(!$_GET['sortByNum']){
-  $sortByNum = 'asc';
-}
-else{
-  $sortByNum = $_GET['sortByNum'];
-}
-if(!$_GET['sortByArtist']){
-  $sortByArtist = 'null';
-}
-else{
-  $sortByArtist = $_GET['sortByArtist'];
-}
+
+    if(!$_GET['sortBy']){
+        $sortBy = 'song_no';
+    }   
+    else{
+        $sortBy = $_GET['sortBy'];
+    }
+
+    if(!$_GET['order']){
+        $order = 'asc';
+    }
+    else{
+        $sortBy = $_GET['order'];
+    }
+    
+    if(!$_GET['filterByArtist']){
+        $filter_artist = 'null';
+    }
+    else{
+        $filter_artist = $_GET['filterByArtist'];
+    }
+
+
 for($j = count($number)-1; $j>-1; $j--){
   $pagenumber = $number[$j] - 1;
   //echo "<a href='./manage.html?page=$pagenumber' class='page'> $number[$j] </a>";
-  echo "<button type='button' onclick=\"location.href='./manage.html?page=$pagenumber&sortByNum=$sortByNum&sortByArtist=$sortByArtist'\" class='page'> $number[$j] </button>";
+  echo "<button type='button' onclick=\"location.href='./manage.html?page=$pagenumber&sortBy=$sortBy&order=$order&filterByArtist=$filterByArtist'\" class='page'> $number[$j] </button>";
 }
 
   
