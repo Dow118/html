8987ca49cbfd204254a5d8e4eab01a6b42bbbd6b
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-05 13:11:26 +0900

    Update manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -7,7 +7,6 @@
                         $page_num = $page*20;
                         $data_num = 20;
         
-                        $result = mysqli_query($connect,"select * from info limit $page_num,$data_num");
                         echo "<table class='type03' style='table-layout:fixed'><tr><th class='small'>
                                 <span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none';>번  호</span><div style='display: none'><HR> <input type='checkbox'>오름차순<input type='checkbox'>내림차순<HR></div></th>
                                 <th><span style='CURSOR: hand' onclick=this.nextSibling.style.display=(this.nextSibling.style.display=='none')?'block':'none';>아티스트</span><div style='display: none'><HR> <input type='checkbox'>오름차순<input type='checkbox'>내림차순<HR></div></th>
@@ -15,6 +14,9 @@
                                 <th>주소값</th>
                                 <th class='small'>장 르</th>
                                 <th>등록일자</th></tr>";
+        
+                        $result = mysqli_query($connect,"select * from info limit $page_num,$data_num");
+        
                           while($row=mysqli_fetch_row($result)){
                           echo "<form method='POST' action = './delete_action.php'>";
                           echo "<input type='hidden' name='key' value='$row[0]'/>";  
