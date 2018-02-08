ede078d797ffa08b976308f9e0440cda2ba3254e
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-08 15:51:57 +0900

    Update manage_page_cal.php

--- admin/menu/manage_page_cal.php
+++ admin/menu/manage_page_cal.php
@@ -42,11 +42,24 @@ while($total>-1){
     else{
         $filter_category = $_GET['filterByCategory'];
     }
+    if(!$_GET['filterByDate_start']){
+        $filter_startdate = "2018-01-01";
+    }
+    else{
+        $filter_startdate = $_GET['filterByDate_start'];
+    }
+
+    if(!$_GET['filterByDate_finish']){
+        $filter_finishdate = date("Y-m-d");
+    }
+    else{
+        $filter_finishdate = $_GET['filterByDate_finish'];
+    }
 
 for($j = count($number)-1; $j>-1; $j--){
   $pagenumber = $number[$j] - 1;
   //echo "<a href='./manage.html?page=$pagenumber' class='page'> $number[$j] </a>";
-  echo "<button type='button' onclick=\"location.href='./manage.html?page=$pagenumber&sortBy=$sortBy&order=$order&filterByArtist=$filter_artist&filterByCategory=$filter_category'\" class='page'> $number[$j] </button>";
+  echo "<button type='button' onclick=\"location.href='./manage.html?page=$pagenumber&sortBy=$sortBy&order=$order&filterByArtist=$filter_artist&filterByCategory=$filter_category&filterByDate_start=$filter_startdate&filterByDate_finish=$filter_finishdate'\" class='page'> $number[$j] </button>";
 }
 
   
