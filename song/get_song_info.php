adc78d0f7cd98676789dbd0e9c59d28689359162
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-02-12 18:47:17 +0900

    Add files via upload

--- song/get_song_info.php
+++ song/get_song_info.php
@@ -1,23 +1,12 @@
 <?php
-    $data = array();
-
-    $song_name = $_GET['song_name'];
-
-    include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
-
-    $query = "SELECT song_artist,song_url FROM info WHERE song_name = '$song_name'";
-
-    if($result = mysqli_query($connect,$query)) {
-        $song = mysqli_fetch_row($result);
-        $data['status'] = 'ok';
-        $data['artist'] = $song[0];
-        $data['url']    = $song[1];
-    }else{
-        $data['status'] = 'err';
-        $data['artist'] = '';
-        $data['url']    = '';
-    }
-
-    echo json_encode($data);    
-
+    
+    $sql = 'SELECT song_artist,song_name,song_url FROM info WHERE song_no='.$_GET['id'];
+    $result = mysqli_query($connect,$sql);
+    $row = mysqli_fetch_assoc($result);
+    echo "<h1>".$row['song_artist']." - ".$row['song_name']."</h1>";
+    echo "<iframe width='680' height='360' src='https://www.youtube.com/embed/".$row['song_url']."'frameborder='0' allowfullscreen></iframe>"."<br><br>";
+    echo "<a href='https://www.youtube.com/watch?v=".$row['song_url']."'target='_blank'>&#60;Youtube에서 보기&#62;</a><br><br>";
+    echo "<form method='get'><input type='hidden' name='song' value='".$row['song_artist']."'>";
+    echo "<input type='submit' id='back' value='검색목록으로'></form>";
+    
 ?>
