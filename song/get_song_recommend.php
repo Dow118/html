4f66c74055405661a3ece8acd389db439f7c296f
LeBlanc7 <35480983+LeBlanc7@users.noreply.github.com>
2018-02-12 19:07:48 +0900

    Add files via upload

--- song/get_song_recommend.php
+++ song/get_song_recommend.php
@@ -17,36 +17,35 @@
         if($song['no'] == $_GET['id'])
             $temp = $song;
     }
-        if(count($data)>3){
-            for($i = 0; $i < 3; $i++){
-                $arr[$i] = mt_rand(0,count($data)-1);
-                for($j = 0; $j < $i; $j++){
-                    if(($arr[$i] == $arr[$j]) || ($data[$arr[$i]]['name'] == $temp['name'])){
-                        $i = $i - 1;
-                        break;
-                    }
+    if(count($data)>3){
+        for($i = 0; $i < 3; $i++){
+            $arr[$i] = mt_rand(0,count($data)-1);
+            for($j = 0; $j < $i; $j++){
+                if(($arr[$i] == $arr[$j]) || ($data[$arr[$i]]['name'] == $temp['name'])){
+                    $i = $i - 1;
+                    break;
                 }
             }
-            for($i = 0; $i < 3; $i++){
-                echo "<div class='wrap'>";
-                echo "<div class='image'><a href='./main.html?id=".$data[$arr[$i]]['no']."'>";
-                echo "<img src='https://img.youtube.com/vi/".$data[$arr[$i]]['url']."/default.jpg' class='img' name='".$data[$arr[$i]]['name']."'></a></div>";
-                echo "<div class='text'><a href='./main.html?id=".$data[$arr[$i]]['no']."'>".$data[$arr[$i]]['artist']." - ".$data[$arr[$i]]['name']."</a>";
-                echo "<hr size='1'>";
-                echo "<a href='https://www.youtube.com/watch?v=".$data[$arr[$i]]['url']."'target='_blank'>&#60;Youtube에서 보기&#62;</a></div></div>";
-            }
         }
-        else if(count($data)<4 && count($data)>1){
-            for($i = 0; $i < count($data); $i++){
-                if($data[$i]['name'] == $temp['name'])
-                continue;
-                echo "<div class='wrap'>";
-                echo "<div class='image'><a href='./main.html?id=".$data[$arr[$i]]['no']."'>";
-                echo "<img src='https://img.youtube.com/vi/".$data[$i]['url']."/default.jpg' class='img' name='".$data[$i]['name']."'></a></div>";
-                echo "<div class='text'><a href='./main.html?id=".$data[$i]['no']."'>".$data[$i]['artist']." - ".$data[$i]['name']."</a>";
-                echo "<hr size='1'>";
-                echo "<a href='https://www.youtube.com/watch?v=".$data[$i]['url']."'target='_blank'>&#60;Youtube에서 보기&#62;</a></div></div>";
-            }
+        for($i = 0; $i < 3; $i++){
+            echo "<div class='wrap'>";
+            echo "<div class='image'><a href='./main.html?id=".$data[$arr[$i]]['no']."'>";
+            echo "<img src='https://img.youtube.com/vi/".$data[$arr[$i]]['url']."/default.jpg' class='img' name='".$data[$arr[$i]]['name']."'></a></div>";
+            echo "<div class='text'><a href='./main.html?id=".$data[$arr[$i]]['no']."'>".$data[$arr[$i]]['artist']." - ".$data[$arr[$i]]['name']."</a>";
+            echo "<hr size='1'>";
+            echo "<a href='https://www.youtube.com/watch?v=".$data[$arr[$i]]['url']."'target='_blank'>&#60;Youtube에서 보기&#62;</a></div></div>";
         }
     }
+    else if(count($data)<4 && count($data)>1){
+        for($i = 0; $i < count($data); $i++){
+            if($data[$i]['name'] == $temp['name']) continue;
+            echo "<div class='wrap'>";
+            echo "<div class='image'><a href='./main.html?id=".$data[$arr[$i]]['no']."'>";
+            echo "<img src='https://img.youtube.com/vi/".$data[$i]['url']."/default.jpg' class='img' name='".$data[$i]['name']."'></a></div>";
+            echo "<div class='text'><a href='./main.html?id=".$data[$i]['no']."'>".$data[$i]['artist']." - ".$data[$i]['name']."</a>";
+            echo "<hr size='1'>";
+            echo "<a href='https://www.youtube.com/watch?v=".$data[$i]['url']."'target='_blank'>&#60;Youtube에서 보기&#62;</a></div></div>";
+        }
+    }
+    
 ?>
