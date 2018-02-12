9c0e26a16b5e8ee75849add120448935f2539330
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-12 17:14:43 +0900

    Update manage_page.php

--- admin/menu/manage_page.php
+++ admin/menu/manage_page.php
@@ -107,11 +107,11 @@
                           echo "<form method='POST' action = './delete_action.php'>";
                           echo "<input type='hidden' name='key' value='$row[0]'/>";  
                           echo "<tr><td class='small'>$row[0] </td><td> $row[1] </td><td class='large'> $row[2] </td><td> $row[3] </td><td class='small'> $row[4] </td><td> $row[5] </td>";
-                          echo "<td><input type='submit' value='delete'  class='button' id='action_delete' src='/imgsrc/delete.png'></td>";
+                          echo "<td><input type='image' value='delete'  class='button' id='action_delete' src='/imgsrc/delete.png'></td>";
                           echo "</form>";
                           echo "<form method='POST' action = './modify.html'>";
                           echo "<input type='hidden' name='key' value='$row[0]'>";
-                          echo "<td><input type='submit' value='modify'  class='button' id='action_modify' src='/imgsrc/edit.png'></td>";
+                          echo "<td><input type='image' value='modify'  class='button' id='action_modify' src='/imgsrc/edit.png'></td>";
                           echo "</form></tr>";
                           }
                         echo "</table>";
