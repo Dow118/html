9ca28d556eeddf12e876490880c6f223f88f4c8e
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-02-20 18:55:08 +0900

    Update validcheck.js

--- script/validcheck.js
+++ script/validcheck.js
@@ -4,6 +4,13 @@ function validcheck(){
   
   if(form.song_name.value==""){
     alert("곡 제목은 필수입력사항입니다");
-    location.reload();
+    return false;
+  }
+  else if(form.song_artist.value==""){
+    alert("아티스트는 필수입력사항입니다");
+    return false;
+  }
+  else{
+    return true;
   }
 }
