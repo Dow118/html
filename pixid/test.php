d09c24b991074b4f7c49fc7ed0eb19bfcdaaa08c
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-04-12 14:12:31 +0900

    Update test.php

--- pixid/test.php
+++ pixid/test.php
@@ -3,7 +3,9 @@
 
 include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
 
-$result = mysqli_query($connect, "select json_extract(data, '$.name') from pixid;");
+//$result = mysqli_query($connect, "select json_extract(data, '$.name') from pixid;");
+
+$result = mysqli_query($connect, "select * from pixid;");
  
 while($row = mysqli_fetch_row($result)){
      $name = $row[0];
