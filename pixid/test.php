669f3924464a968cd994d8b13e53f3df88729708
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-04-12 14:06:21 +0900

    Create test.php

---
+++ pixid/test.php
@@ -0,0 +1,14 @@
+
+<? php
+
+include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
+
+$result = mysqli_query($connect, "select json_extract(data, '$.name') from pixid;");
+ 
+while($row = mysqli_fetch_row($result)){
+     $name = $row[0];
+}
+
+echo $name;
+
+?>
