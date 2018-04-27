252feb318b6118d3adae4f5f32a89015be6d3eb3
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-04-27 12:50:16 +0900

    Update test.php

--- pixid/test.php
+++ pixid/test.php
@@ -3,14 +3,22 @@
 
 include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
 
-//$result = mysqli_query($connect, "select json_extract(data, '$.name') from pixid;");
+$resultR = mysqli_query($connect, "select json_extract(data, '$.bodyR') from pixid;");
+$resultG = mysqli_query($connect, "select json_extract(data, '$.bodyG') from pixid;");
+$resultB = mysqli_query($connect, "select json_extract(data, '$.bodyB') from pixid;");
 
-$result = mysqli_query($connect, "select * from pixid;");
+
+//$result = mysqli_query($connect, "select * from pixid;");
  
-while($row = mysqli_fetch_row($result)){
-     $name = $row[0];
+while($row = mysqli_fetch_row($resultR)){
+     $R = $row[0];
 }
-
-echo $name;
+while($row = mysqli_fetch_row($resultG)){
+     $G = $row[0];
+}
+while($row = mysqli_fetch_row($resultB)){
+     $B = $row[0];
+}
+echo $R.$G.$B;
 
 ?>
