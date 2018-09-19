547466003e3c04ba61bbef5f18301dcdbd32c1b0
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-09-19 17:48:45 +0900

    Update postTest.php

--- pixid/postTest.php
+++ pixid/postTest.php
@@ -8,7 +8,7 @@ $char = $_POST['char'];
 include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
 
 $result = mysqli_query($connect,"insert into UOSRunner(user_id, user_score, user_date, user_char)
- value($id, $score, default, $char)");
+ value('$id', '$score', default, '$char')");
 
 if($result)
   echo "OK!"; 
