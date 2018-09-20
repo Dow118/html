443053fba5342ce8f1f734f6ec7ec5db24a1e060
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-09-20 16:57:13 +0900

    Update idCheck.php

--- UOSRunner/idCheck.php
+++ UOSRunner/idCheck.php
@@ -1,5 +1,15 @@
 <?php
 
+$id = $_POST['id'];
+
+include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
+
+$result = mysqli_query($connect, "select COUNT(user_id) from UOSRunner where user_id='$id'");
+
+if($result == 0)
+  echo "able"; 
+else
+  echo "unable";
 
 
 ?>
