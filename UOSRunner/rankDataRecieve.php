476c24be1a7ddf3955ef76dd667cf5b6a20863e0
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-09-23 16:38:09 +0900

    Update rankDataRecieve.php

--- UOSRunner/rankDataRecieve.php
+++ UOSRunner/rankDataRecieve.php
@@ -5,17 +5,7 @@ $id = $_POST['id'];
 
 include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
 
-$result = mysqli_query($connect,"SELECT
-    user_id, user_score, user_character,
-    ( @real_rank := IF ( @last > user_score, @real_rank:=@real_rank+1, @real_rank ) ) AS real_rank,
-    ( @last := user_score )
-FROM
-    UOSRunner AS a ,
-    ( SELECT @last := 0, @real_rank := 1 ) AS b 
-ORDER BY
-    a.user_score DESC
-LIMIT
-    0,10;");
+$result = mysqli_query($connect,"SELECT * from UOSRunner");
 
 
 if($result){
