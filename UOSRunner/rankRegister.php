6df67ba03956d663fd7ea30856b2e470cf7e9870
Dow118 <35480079+Dow118@users.noreply.github.com>
2018-10-07 19:16:54 +0900

    Update rankRegister.php

--- UOSRunner/rankRegister.php
+++ UOSRunner/rankRegister.php
@@ -5,11 +5,21 @@ $character = $_POST['character'];
 $score = $_POST['score'];
 
 include $_SERVER['DOCUMENT_ROOT'].'/dbconnect.php';
-$result = mysqli_query($connect, "update UOSRunner set user_character='$character', user_score='$score' where user_id='$id'");
-if($result){
-    echo "complete";
+
+// 1. check if the score is BEST SCORE
+$result = mysqli_query($connect, "select user_score from UOSRunner where user_id='$id'");
+$r = mysqli_fetch_row($result);
+
+if((int)$r <= (int)$score){
+    // 2. if BEST SCORE, register it.
+    $result = mysqli_query($connect, "update UOSRunner set user_character='$character', user_score='$score' where user_id='$id'");
+    
+    if($result)
+        echo "complete";
+    else
+        echo "ERROR";
 }
-else
-  echo "ERROR";
-  
+else{
+    
+}
 ?>
